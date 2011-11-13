efce81c149667b93090f720b4faaf15659773aed
Gang Yu <yugang.bj@gmail.com>
2011-11-13 13:00:24 +0800

    update bug597-31.c

--- asm_p_constraint/bug597-31.c
+++ asm_p_constraint/bug597-31.c
@@ -1,6 +1,6 @@
 int main(void)
 {
   void * ret__;
-  asm volatile ("mov" " "":%P" "1"",%0" : "=r" (ret__) : "p" (0xffffffff00000000));
+  asm volatile ("mov" " ""%P" "1"",%0" : "=r" (ret__) : "p" (0xffffffff00000000));
   return 0;
 }
