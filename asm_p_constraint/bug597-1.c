36a22fe332f762e1911a272598b9462e76b65e0c
Gang Yu <yugang.bj@gmail.com>
2011-11-13 12:50:47 +0800

    README updated

--- asm_p_constraint/bug597-1.c
+++ asm_p_constraint/bug597-1.c
@@ -3,6 +3,6 @@ void *  per_cpu__current_task;
 int main(void)
 {
   void * ret__;
-  asm volatile ("mov" "q ""%%""gs"":%P" "1"",%0" : "=r" (ret__) : "p" (per_cpu__current_task));
+  asm volatile ("mov %P" "1"",%0" : "=r" (ret__) : "p" (per_cpu__current_task));
   return 0;
 }
--- asm_p_constraint/bug597-4.c
+++ asm_p_constraint/bug597-4.c
@@ -1,8 +1,7 @@
-extern __attribute__((section(".data.percpu" ""))) __typeof__(struct task_struct *) per_cpu__current_task;
-
-extern void main(void)
+int main(void)
 {
   void * ret__;
-  int u;
+  int u=3;
   asm volatile ("mov" "q ""%%""gs"":%P" "1"",%0" : "=r" (ret__) : "p" (&u));
+  return 0;
 }
